0c9dfa49330f300a593c96a52e0ee279cb680aa1
RuefulMage <feonorabd2000@gmail.com>
2020-06-20 14:27:30 +0300

    jQuery to webpack config added

--- webpack-config/webpack.base.config.js
+++ webpack-config/webpack.base.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin} = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './src/js/index.js',
@@ -52,6 +53,10 @@ module.exports = {
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
             filename: "./css/[name].css"
-        })
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
     ]
 };
