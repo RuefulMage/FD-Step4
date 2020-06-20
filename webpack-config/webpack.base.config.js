6312f597c644d3e1d4e2929a42444dccbf92fc60
RuefulMage <feonorabd2000@gmail.com>
2020-06-20 14:22:50 +0300

    webpack configuration added: babel, css, scss, js loaders, postscc loader and added configurations for production and development modes

---
+++ webpack-config/webpack.base.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: "./js/main.js",
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: "/",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use:[
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: "postcss-loader",
+                        options: {config: {path: 'webpack-config/postcss.config.js'}}
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use:[
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: "postcss-loader",
+                        options: {config: {path: 'webpack-config/postcss.config.js'}}
+                    },
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+        ]
+    },
+    plugins :[
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "./css/[name].css"
+        })
+    ]
+};
