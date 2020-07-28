0ab51648997cae552f174fb0e72ff73c8bbf62e9
RuefulMage <feonorabd2000@gmail.com>
2020-07-28 12:15:33 +0300

    tests for horizontal orientation behavior added

---
+++ src/js/View/OrientationBehaviors/HorizontalOrientationBehavior.test.ts
@@ -0,0 +1,74 @@
+import { HorizontalOrientationBehavior } from './HorizontalOrientationBehavior';
+
+describe('Horizontal orientation behavior class', function() {
+
+    let domElement;
+    let horizontalOrientationBehavior: HorizontalOrientationBehavior;
+    beforeEach(function() {
+        domElement = {
+            parentElement: {
+                offsetWidth: 100,
+                getBoundingClientRect(){
+                    return {
+                      left: 10,
+                      right: 15
+                    };
+                }
+            },
+            style: {
+                left: '',
+                right: ''
+            },
+            offsetWidth: 40
+        }
+        horizontalOrientationBehavior = new HorizontalOrientationBehavior();
+    });
+
+    describe('Set position', function() {
+        test('Should shift input position to half of element width and set left attribute of ' +
+            'input DOM element to shifted position', function() {
+            let inputPosition = 10;
+            horizontalOrientationBehavior.setPosition(inputPosition, domElement);
+
+            expect(domElement.style.left).toBe('-10%');
+        });
+    });
+
+    describe('Get position from coordinates', function(){
+        test('Should return correct position in percents', function(){
+            let clientX = 30;
+            let clientY = 40;
+
+            let outputPosition = horizontalOrientationBehavior.getPositionFromCoordinates(clientX, clientY, domElement);
+
+            expect(outputPosition).toBe(20);
+        });
+    });
+
+
+    describe('Reset styles', function() {
+       test('Should clean style attribute of dom element', function(){
+           let element = document.createElement('div');
+           element.style.width = '100px';
+           element.style.height = '200px';
+           element.style.left = '10%';
+           element.style.top = '20%';
+
+           horizontalOrientationBehavior.resetStyles(element);
+
+           expect(element.getAttribute('style')).toBe('');
+       });
+    });
+
+    describe('Set range position', function(){
+        test('Should set styles of element to input left and right position', function(){
+            let minEdge = 20;
+            let maxEdge = 65;
+            horizontalOrientationBehavior.setRangePositions(minEdge, maxEdge, domElement);
+
+            expect(domElement.style.left).toBe('20%');
+            expect(domElement.style.right).toBe('35%');
+
+        });
+    });
+});
