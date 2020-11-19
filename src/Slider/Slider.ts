4fc8c07bcd884cf58d64b94137039346f813fc31
RuefulMage <feonorabd2000@gmail.com>
2020-11-19 15:07:07 +0300

    fixed necessity to convert string to Orientation in options, at slider creation moment

--- src/Slider/Slider.ts
+++ src/Slider/Slider.ts
@@ -7,113 +7,121 @@ import Orientation from './Utils/Orientation';
 import Logger from './Utils/Logger';
 
 class Slider implements IObserver {
-    protected view: View;
+  protected view: View;
 
-    protected model: Model;
+  protected model: Model;
 
-    protected controller: Controller;
+  protected controller: Controller;
 
-    protected rootElement: HTMLElement;
+  protected rootElement: HTMLElement;
 
-    constructor(rootElement: HTMLElement, options: SliderOptions) {
-      this.rootElement = rootElement;
-      this.model = new Model(options);
-      this.model.attach(this);
-      this.view = new View(rootElement, options);
-      this.view.attach(this);
-      this.controller = new Controller(this.view, this.model, options.isRange);
+  constructor(rootElement: HTMLElement, options: SliderOptions) {
+    this.rootElement = rootElement;
+    this.model = new Model(options);
+    this.model.attach(this);
+
+    const viewOptions: {
+      orientation?: Orientation, isRange?: boolean, isTipsHidden?: boolean
+    } = {
+      orientation: options.orientation as Orientation,
+      isRange: options.isRange,
+      isTipsHidden: options.isTipsHidden,
+    };
+    this.view = new View(rootElement, viewOptions);
+    this.view.attach(this);
+    this.controller = new Controller(this.view, this.model, options.isRange);
+  }
+
+  public isRange(): boolean {
+    return this.model.getRangeStatus();
+  }
+
+  public setRangeMode(isRange: boolean): void {
+    this.model.setRangeMode(isRange);
+  }
+
+  public getMinValue(): number {
+    return this.model.getMinValue();
+  }
+
+  public setMinValue(minValue: number): boolean {
+    try {
+      this.model.setMinValue(minValue);
+      return true;
+    } catch (error) {
+      Logger.logWarning('Model', error.message);
+      return false;
     }
+  }
 
-    public isRange(): boolean {
-      return this.model.getRangeStatus();
-    }
+  public getMaxValue(): number {
+    return this.model.getMaxValue();
+  }
 
-    public setRangeMode(isRange: boolean): void {
-      this.model.setRangeMode(isRange);
+  public setMaxValue(maxValue: number): boolean {
+    try {
+      this.model.setMaxValue(maxValue);
+      return true;
+    } catch (error) {
+      Logger.logWarning('Model', error.message);
+      return false;
     }
+  }
 
-    public getMinValue(): number {
-      return this.model.getMinValue();
-    }
+  public getHighValue(): number {
+    return this.model.getHighValue();
+  }
 
-    public setMinValue(minValue: number): boolean {
-      try {
-        this.model.setMinValue(minValue);
-        return true;
-      } catch (error) {
-        Logger.logWarning('Model', error.message);
-        return false;
-      }
-    }
+  public setHighValue(highValue: number): void {
+    this.model.setHighValue(highValue);
+  }
 
-    public getMaxValue(): number {
-      return this.model.getMaxValue();
-    }
+  public getLowValue(): number {
+    return this.model.getLowValue();
+  }
 
-    public setMaxValue(maxValue: number): boolean {
-      try {
-        this.model.setMaxValue(maxValue);
-        return true;
-      } catch (error) {
-        Logger.logWarning('Model', error.message);
-        return false;
-      }
-    }
+  public setLowValue(lowValue: number): void {
+    this.model.setLowValue(lowValue);
+  }
 
-    public getHighValue(): number {
-      return this.model.getHighValue();
-    }
+  public getStep(): number {
+    return this.model.getStep();
+  }
 
-    public setHighValue(highValue: number): void {
-      this.model.setHighValue(highValue);
+  public setStep(step: number): boolean {
+    try {
+      this.model.setStep(step);
+      return true;
+    } catch (error) {
+      Logger.logWarning('Model', error.message);
+      return false;
     }
+  }
 
-    public getLowValue(): number {
-      return this.model.getLowValue();
-    }
+  public getOrientation(): string {
+    return this.view.getOrientation();
+  }
 
-    public setLowValue(lowValue: number): void {
-      this.model.setLowValue(lowValue);
-    }
+  public setOrientation(orientation: string): void {
+    this.view.setOrientation(orientation as Orientation);
+  }
 
-    public getStep(): number {
-      return this.model.getStep();
-    }
+  public hideTips(): void {
+    this.view.hideTips();
+  }
 
-    public setStep(step: number): boolean {
-      try {
-        this.model.setStep(step);
-        return true;
-      } catch (error) {
-        Logger.logWarning('Model', error.message);
-        return false;
-      }
-    }
+  public showTips(): void {
+    this.view.showTips();
+  }
 
-    public getOrientation(): string {
-      return this.view.getOrientation();
-    }
+  public getHideStatus(): boolean {
+    return this.view.getHideStatus();
+  }
 
-    public setOrientation(orientation: string): void {
-      this.view.setOrientation(orientation as Orientation);
-    }
-
-    public hideTips(): void {
-      this.view.hideTips();
-    }
-
-    public showTips(): void {
-      this.view.showTips();
-    }
-
-    public getHideStatus(): boolean {
-      return this.view.getHideStatus();
-    }
-
-    public update(eventName: string, data?: any): void {
-      const changeEvent = new CustomEvent('slider-change', { bubbles: true, cancelable: true });
-      this.rootElement.dispatchEvent(changeEvent);
-    }
+  public update(eventName: string, data?: any): void {
+    const changeEvent = new CustomEvent('slider-change', { bubbles: true, cancelable: true });
+    this.rootElement.dispatchEvent(changeEvent);
+  }
 }
 
 (function ($) {
--- src/Slider/Utils/SliderOptions.ts
+++ src/Slider/Utils/SliderOptions.ts
@@ -1,14 +1,14 @@
 import Orientation from './Orientation';
 
 type SliderOptions = {
-    isRange: boolean,
-    isTipsHidden: boolean,
-    maxValue: number,
-    minValue: number,
-    orientation: Orientation,
-    startValueHigh: number,
-    startValueLow: number,
-    step: number
+  isRange: boolean,
+  isTipsHidden: boolean,
+  maxValue: number,
+  minValue: number,
+  orientation: string,
+  startValueHigh: number,
+  startValueLow: number,
+  step: number
 };
 
 // eslint-disable-next-line no-undef
