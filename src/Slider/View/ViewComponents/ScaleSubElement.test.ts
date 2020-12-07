cb3d8a55702025eaa0380466cd20db00f0fdff7f
RuefulMage <feonorabd2000@gmail.com>
2020-12-07 15:23:33 +0300

    Scale subelement tests updated

--- src/Slider/View/ViewComponents/ScaleSubElement.test.ts
+++ src/Slider/View/ViewComponents/ScaleSubElement.test.ts
@@ -1,28 +1,28 @@
-// import ScaleSubElement from './ScaleSubElement';
-//
-// describe('Scale subelement class', () => {
-//   let scaleSubElement: ScaleSubElement;
-//   let parentElement;
-//
-//   beforeEach(() => {
-//     parentElement = document.createElement('div');
-//     document.body.append(parentElement);
-//     scaleSubElement = new ScaleSubElement(parentElement, 10);
-//   });
-//
-//   describe('Get position', () => {
-//     test('Should return current position', () => {
-//       expect(scaleSubElement.getPosition()).toBe(10);
-//     });
-//   });
-//
-//   describe('Set position', () => {
-//     test('Should set position to input value and set attribute data-scale-position to new position',
-//       () => {
-//         scaleSubElement.setPosition(60);
-//
-//         expect(scaleSubElement.getPosition()).toBe(60);
-//         expect(scaleSubElement.getDOMNode().getAttribute('data-scale-position')).toBe('60');
-//       });
-//   });
-// });
+import ScaleSubElement from './ScaleSubElement';
+
+describe('Scale subelement class', () => {
+  let scaleSubElement: ScaleSubElement;
+  let parentElement;
+
+  beforeEach(() => {
+    parentElement = document.createElement('div');
+    document.body.append(parentElement);
+    scaleSubElement = new ScaleSubElement(parentElement, 10);
+  });
+
+  describe('Get position', () => {
+    test('Should return current position', () => {
+      expect(scaleSubElement.getPosition()).toBe(10);
+    });
+  });
+
+  describe('Set position', () => {
+    test('Should set position to input value and set attribute data-scale-position to new position',
+      () => {
+        scaleSubElement.setPosition(60);
+
+        expect(scaleSubElement.getPosition()).toBe(60);
+        expect(scaleSubElement.getDOMNode().getAttribute('data-scale-position')).toBe('60');
+      });
+  });
+});
