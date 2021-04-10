5486eeb8506846666247e99fdb935d27d2cc1d10
RuefulMage <feonorabd2000@gmail.com>
2021-04-10 17:18:42 +0300

    file import order changed in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-importAll(require.context('./', true, /((?<!\.test)(\.scss|\.ts))$/));
+importAll(require.context('./slider', true, /((?<!\.test)(\.scss|\.ts))$/));
+importAll(require.context('./demo', true, /((?<!\.test)(\.scss|\.ts))$/));
+importAll(require.context('./', false, /((?<!\.test)(\.scss|\.ts))$/));
 function importAll(context: __WebpackModuleApi.RequireContext) {
   context.keys().forEach(context);
 }
